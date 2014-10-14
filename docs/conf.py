9a7f4714c997a20e7f764289ae94af4fe8ee5868
uzumaxy <uzumaxy@gmail.com>
2014-10-14 10:45:35 +0300

    Tech update.

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sysSx
+import sys
 import os
 
 # If extensions (or modules to document with autodoc) are in another directory,
