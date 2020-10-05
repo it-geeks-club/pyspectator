084bef72ae1f262e7c5642ff5a09940eced932e9
Max <uzumaxy@gmail.com>
2020-10-05 04:59:11 +0300

    Updated deps versions

--- test/test_convert.py
+++ test/test_convert.py
@@ -4,7 +4,7 @@ from pyspectator.convert import UnitByte
 def test_get_name_reduction():
     # Existing reduction
     reduction = UnitByte.get_name_reduction(UnitByte.megabyte)
-    assert reduction is 'MB'
+    assert reduction == 'MB'
     # Non-existing reduction
     reduction = UnitByte.get_name_reduction('Non-existing reduction')
     assert reduction is None
